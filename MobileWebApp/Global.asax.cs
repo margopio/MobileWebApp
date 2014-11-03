2041e852922e6e27fe8d17b053ae1153c844e4c5
margopio <margopio@gmail.com>
2014-11-03 16:32:19 +0800

    Initial commit.

---
+++ MobileWebApp/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApi2;
+
+namespace MobileWebApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();            
+
+            // Register API routes
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
