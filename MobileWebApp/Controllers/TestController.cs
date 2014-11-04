43974a0936ffcb0fa25c1cf1e0afcc59cccdaf39
margopio <margopio@gmail.com>
2014-11-04 15:39:19 +0800

    Test text response 1.

--- MobileWebApp/Controllers/TestController.cs
+++ MobileWebApp/Controllers/TestController.cs
@@ -13,7 +13,7 @@ namespace MobileWebApp.Controllers
         {
             return new HttpResponseMessage()
             {
-                Content = new StringContent("GET: Test message - Remote")
+                Content = new StringContent("GET: Test message - Remote 1")
             };
         }
 
@@ -21,7 +21,7 @@ namespace MobileWebApp.Controllers
         {
             return new HttpResponseMessage()
             {
-                Content = new StringContent("POST: Test message - Remote")
+                Content = new StringContent("POST: Test message - Remote 1")
             };
         }
 
@@ -29,7 +29,7 @@ namespace MobileWebApp.Controllers
         {
             return new HttpResponseMessage()
             {
-                Content = new StringContent("PUT: Test message - Remote")
+                Content = new StringContent("PUT: Test message - Remote 1")
             };
         }
     }
