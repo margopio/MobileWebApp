af883ec850a3760e9ed44858c36e6547aa213fbe
margopio <margopio@gmail.com>
2015-08-31 14:49:38 +0800

    Change push timer setting to 3 mins.

--- MobileWebApp/EchoHub.cs
+++ MobileWebApp/EchoHub.cs
@@ -14,7 +14,7 @@ namespace MobileWebApp
             while (true)
             {
                 Clients.All.showTime(DateTime.Now.ToLongTimeString());
-                Thread.Sleep(TimeSpan.FromMinutes(2));
+                Thread.Sleep(TimeSpan.FromMinutes(3));
                 //Thread.Sleep(new TimeSpan(0, 10, 0));
             }
         }
